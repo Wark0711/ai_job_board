942d6eff49bfb942be1cbf229358af3f9561a341
Wark0711 <mickey.rahul98@gmail.com>
2025-06-21 18:40:44 +0530

    Worked on sidebar section in homepage

---
+++ src/features/user/components/SidebarUserButton.tsx
@@ -0,0 +1,18 @@
+import { auth } from "@clerk/nextjs/server";
+import { Suspense } from "react";
+import { SidebarUserButtonClient } from "./_SidebarUserButtonClient";
+
+export function SidebarUserButton() {
+    return (
+        <Suspense>
+            <SidebarUserSuspense />
+        </Suspense>
+    )
+}
+
+async function SidebarUserSuspense() {
+
+    const { userId } = await auth()
+
+    return <SidebarUserButtonClient user={{ email: 'test@test.com', name: 'Test User', imageUrl: '' }} />
+}
